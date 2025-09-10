6b97b9c530b44c6af18b2cade9b6303e181560f4
jiangbl <1632504075@qq.com>
2025-09-10 16:57:27 +0800

    feat(sysinfo.c):add user command sysinfo implement

--- user/sysinfo.c
+++ user/sysinfo.c
@@ -7,5 +7,12 @@
 int
 main(int argc, char *argv[])
 {
+    struct sysinfo info;
+    if(sysinfo(&info) < 0){
+        fprintf(2, "sysinfo error\n");
+        exit(1);
+    }
+    printf("freemem: %d bytes\n", info.freemem);
+    printf("nproc: %d\n", info.nproc);
     exit(0);
 }
