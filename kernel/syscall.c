5749cb1402ffaf13d9cb43df62deddf7433880a3
jiangbl <1632504075@qq.com>
2025-09-10 14:24:42 +0800

    fix(trace):output syscall return value instead of syscall number

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -167,11 +167,12 @@ syscall(void)
   release(&p->lock);
 
   num = p->trapframe->a7;
-  if(mask & (1 << num)) {
-    printf("%d: syscall	%s -> %d\n", p->pid, syscallname[num], num);
-  }
+
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num](); 
+    if(mask & (1 << num)) {
+      printf("%d: syscall	%s -> %d\n", p->pid, syscallname[num], p->trapframe->a0);
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
