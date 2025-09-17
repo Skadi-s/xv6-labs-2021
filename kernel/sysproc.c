6ab6479e17206efbd5fc4fb6af932429fb75f5ea
jiangbl <1632504075@qq.com>
2025-09-17 15:50:08 +0800

    fix(sys_sleep):change the backtrace call position

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -67,10 +67,10 @@ sys_sleep(void)
       release(&tickslock);
       return -1;
     }
-    backtrace();
     sleep(&ticks, &tickslock);
   }
   release(&tickslock);
+  backtrace();
   return 0;
 }
 
