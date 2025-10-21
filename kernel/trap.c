5dd1d2d07041d0b5c0a937314677a8d1884184a6
TreeSharkMac <1632504075@qq.com>
2025-10-21 11:10:31 +0800

    move log message to handle_user_page_fault in vm.c

--- kernel/trap.c
+++ kernel/trap.c
@@ -69,7 +69,6 @@ usertrap(void)
     // handle page fault
     uint64 va = r_stval();
     if (handle_user_page_fault(p, va) < 0) {
-      printf("usertrap(): page fault handle failed pid=%d va=%p\n", p->pid, va);
       p->killed = 1;
     }
   }else if((which_dev = devintr()) != 0){
